65389892b42d48f9faf45b304c301eb4e3d75a74
Jacek Puchta <jpa.puchta@gmail.com>
2014-01-18 13:51:54 +0100

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Zweeter2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zweeter2::Application.config.secret_key_base = '227bc5d7cbc77b9d087a5ff601e3e029731c52a75f09b58a70c6980fac8905bf170fec839ef889419e3088d25e22e7271ed2353e433bc623891128ab345171c8'
